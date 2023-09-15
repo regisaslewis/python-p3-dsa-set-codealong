bdec31a995e51df3213696cebcca3696bc18f583
RegisLewis <regisaslewis@gmail.com>
2023-09-15 16:42:52 -0700

    data structure code along

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,28 @@
 class MySet:
+    def __init__(self, enumerable = []):
+        self.dictionary = {}
+        for n in enumerable:
+            self.dictionary[n] = True
 
-    pass
+    def has(self, value):
+        return value in self.dictionary
+    
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+    
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+    
+    def size(self):
+        return len(self.dictionary)
+    
+    def __str__(self):
+        set = [str(n) for n in self.dictionary]
+        return "MySet: {" + ",".join(set) + "}"
+    
+    def clear(self):
+        for n in list(self.dictionary):
+            self.dictionary.pop(n)
+        return self.dictionary
